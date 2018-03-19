40ff916241fbde78ccbe180a63769eb58f1d1545
treyshel <tshelton@basecampcodingacademy.org>
2018-03-19 11:43:52 -0500

    forgot to add the username and password columns in the statements at the bottom of my file

--- backend/backend/src/main/java/com/example/on-belay-tables.sql
+++ backend/backend/src/main/java/com/example/on-belay-tables.sql
@@ -19,7 +19,7 @@ CREATE TABLE student_desc(
 );
 
 INSERT INTO students(id, f_name, l_name, username, p_word, email) VALUES (1, 'Trey', 'Shelton', 'treyshel', 'BASECAMP', 'tshelton@basecampcodingacademy.org');
-INSERT INTO students(id, f_name, l_name, email) VALUES (2, 'Vale', 'Alvarez', 'valvarez', 'papaV', 'valvarez@basecampcodingacademy.org');
+INSERT INTO students(id, f_name, l_name, username, p_word, email) VALUES (2, 'Vale', 'Alvarez', 'valvarez', 'papaV', 'valvarez@basecampcodingacademy.org');
 
 INSERT INTO student_desc(id, linkedin_url, resume_url, github_url, portfolio_url) VALUES (1, 'https://www.linkedin.com/feed/', 'https://www.resume.com/trey/', 'https://www.github.com/treyshel/', 'https://www.treyshel.github.io');
 INSERT INTO student_desc(id, linkedin_url, resume_url, github_url, portfolio_url) VALUES (2, 'https://www.linkedin.com/feed/', 'https://www.resume.com/vale/', 'https://www.github.com/vale/', 'https://www.valvarez.github.io');

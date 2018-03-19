d07a8d3f17085de32c70a3d6f3efe4f18bd08f1a
treyshel <tshelton@basecampcodingacademy.org>
2018-03-18 22:42:16 -0500

    added some insert statements to have some data in my backend

--- backend/backend/src/main/java/com/on-belay-tables.sql
+++ backend/backend/src/main/java/com/on-belay-tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE students (
     id Serial Unique Primary Key,
     f_name text,
     l_name text,
-        email text, 
+    email text, 
 
 );
 
@@ -16,3 +16,8 @@ CREATE TABLE student_desc (
     github_url text,
     portfolio_url text
 );
+
+INSERT INTO students(f_name, l_name) VALUES ('Trey', 'Shelton');
+INSERT INTO students(f_name, l_name) VALUES ('Vale', 'Alvarez');
+INSERT INTO students(linkedin_url, resume_url, github_url, portfolio_url) VALUES ('https://www.linkedin.com/feed/', 'https://www.resume.com/trey/', 'https://www.github.com/treyshel/', 'https://www.treyshel.github.io');
+INSERT INTO students(linkedin_url, resume_url, github_url, portfolio_url) VALUES ('https://www.linkedin.com/feed/', 'https://www.resume.com/vale/', 'https://www.github.com/vale/', 'https://www.valvarez.github.io');

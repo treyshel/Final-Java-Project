9ccd4e5b3556ac5225cb9de55fa24c5e9b38bce2
treyshel <tshelton@basecampcodingacademy.org>
2018-03-19 11:33:58 -0500

    created returnAllUsers()

--- backend/backend/src/main/java/com/example/backend/StudentRepostiory.java
+++ backend/backend/src/main/java/com/example/backend/StudentRepostiory.java
@@ -1,4 +1,27 @@
 package com.example.backend;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class StudentRepostiory {
+
+
+    public static ArrayList<Student> returnAllUsers() {
+        try {
+            Connection con = Connect.connectDB();
+            PreparedStatement preparedStatement = con.prepareStatement("SELECT * FROM players;");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            ArrayList<Student> students = new ArrayList<Student>();
+            while(resultSet.next()){
+                students.add(new Student(resultSet.getInt("id"),resultSet.getString("f_name"), resultSet.getString("l_name"), resultSet.getString("email")));
+            }
+            return students;
+        } catch (SQLException ex){
+            System.out.println(ex.getMessage());
+            return null;
+        }
+    }
 }

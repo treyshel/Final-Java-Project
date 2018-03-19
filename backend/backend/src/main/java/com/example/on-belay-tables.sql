739511bb4a4c050ef32ac45ca58e27dfcbcdeccb
treyshel <tshelton@basecampcodingacademy.org>
2018-03-19 11:42:18 -0500

    added a username and password to the students table

--- backend/backend/src/main/java/com/example/on-belay-tables.sql
+++ backend/backend/src/main/java/com/example/on-belay-tables.sql
@@ -3,6 +3,8 @@ CREATE TABLE students(
     id Serial Unique Primary Key,
     f_name text,
     l_name text,
+    username text,
+    p_word text,
     email text 
 );
 
@@ -16,8 +18,8 @@ CREATE TABLE student_desc(
     portfolio_url text
 );
 
-INSERT INTO students(id, f_name, l_name, email) VALUES (1, 'Trey', 'Shelton', 'tshelton@basecampcodingacademy.org');
-INSERT INTO students(id, f_name, l_name, email) VALUES (2, 'Vale', 'Alvarez', 'valvarez@basecampcodingacademy.org');
+INSERT INTO students(id, f_name, l_name, username, p_word, email) VALUES (1, 'Trey', 'Shelton', 'treyshel', 'BASECAMP', 'tshelton@basecampcodingacademy.org');
+INSERT INTO students(id, f_name, l_name, email) VALUES (2, 'Vale', 'Alvarez', 'valvarez', 'papaV', 'valvarez@basecampcodingacademy.org');
 
 INSERT INTO student_desc(id, linkedin_url, resume_url, github_url, portfolio_url) VALUES (1, 'https://www.linkedin.com/feed/', 'https://www.resume.com/trey/', 'https://www.github.com/treyshel/', 'https://www.treyshel.github.io');
 INSERT INTO student_desc(id, linkedin_url, resume_url, github_url, portfolio_url) VALUES (2, 'https://www.linkedin.com/feed/', 'https://www.resume.com/vale/', 'https://www.github.com/vale/', 'https://www.valvarez.github.io');

533f47a0495907ad5ef2c25698eee51476aa46e3
treyshel <tshelton@basecampcodingacademy.org>
2018-03-18 22:31:53 -0500

    made a new table that stores urls that I want in my database

--- backend/backend/src/main/java/com/on-belay-tables.sql
+++ backend/backend/src/main/java/com/on-belay-tables.sql
@@ -1,14 +1,18 @@
 --table for the basecamp students
 CREATE TABLE students ( 
-    id Serial Primary Key,
+    id Serial Unique Primary Key,
     f_name text,
     l_name text,
-    email text, 
+        email text, 
+
 );
 
 
---
+--student's contact/work
 CREATE TABLE student_desc (
-    id Serial Primary Key,
-    
-)
+    id Serial Primary Key,    email text, 
+    linkedin_url text,
+    resume_url text,
+    github_url text,
+    portfolio_url text
+);

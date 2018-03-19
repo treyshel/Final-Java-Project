08b548894df17d831eb4c0a1edd3f2c913db2fcf
treyshel <tshelton@basecampcodingacademy.org>
2018-03-18 22:12:26 -0500

    made my table for the database that stores the student's firstname, lastname, and email

---
+++ backend/backend/src/main/java/com/on-belay-tables.sql
@@ -0,0 +1,14 @@
+--table for the basecamp students
+CREATE TABLE students ( 
+    id Serial Primary Key,
+    f_name text,
+    l_name text,
+    email text, 
+);
+
+
+--
+CREATE TABLE student_desc (
+    id Serial Primary Key,
+    
+)

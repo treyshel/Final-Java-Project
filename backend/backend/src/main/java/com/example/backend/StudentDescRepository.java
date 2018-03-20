588a4b7eaedf8f3a5727c6b29ce64efcafee5049
treyshel <tshelton@basecampcodingacademy.org>
2018-03-20 13:41:27 -0500

    changed the interests column to a desired_location column

--- backend/backend/src/main/java/com/example/backend/StudentDescRepository.java
+++ backend/backend/src/main/java/com/example/backend/StudentDescRepository.java
@@ -2,4 +2,5 @@ package com.example.backend;
 
 public class StudentDescRepository {
 
+
 }
--- backend/backend/src/main/java/com/example/on-belay-tables.sql
+++ backend/backend/src/main/java/com/example/on-belay-tables.sql
@@ -26,7 +26,7 @@ CREATE TABLE student_achievements(
     programming_langs text,
     bio text,
     academics text,
-    interests text
+    desired_location text
 );
 
 INSERT INTO student(id, f_name, l_name, username, p_word, email) VALUES (1, 'Trey', 'Shelton', 'treyshel', 'BASECAMP', 'tshelton@basecampcodingacademy.org');
@@ -35,5 +35,5 @@ INSERT INTO student(id, f_name, l_name, username, p_word, email) VALUES (2, 'Val
 INSERT INTO student_desc(id, student_id, linkedin_url, resume_url, github_url, portfolio_url) VALUES (1, 1, 'https://www.linkedin.com/feed/', 'https://www.resume.com/trey/', 'https://www.github.com/treyshel/', 'https://www.treyshel.github.io');
 INSERT INTO student_desc(id, student_id, linkedin_url, resume_url, github_url, portfolio_url) VALUES (2, 2, 'https://www.linkedin.com/feed/', 'https://www.resume.com/vale/', 'https://www.github.com/vale/', 'https://www.valvarez.github.io');
 
-INSERT INTO student_achievements(id, student_id, programming_langs, bio, academics, interests) VALUES (1, 1, 'python', 'Software Developer', 'Pontotoc High', 'fishing');
-INSERT INTO student_achievements(id, student_id, programming_langs, bio, academics, interests) VALUES (2, 2, 'java', 'Software Engineer', 'LHS', 'cooking');
+INSERT INTO student_achievements(id, student_id, programming_langs, bio, academics, desired_location) VALUES (1, 1, 'python', 'Software Developer', 'Pontotoc High', 'Mississippi');
+INSERT INTO student_achievements(id, student_id, programming_langs, bio, academics, desired_location) VALUES (2, 2, 'java', 'Software Engineer', 'LHS', 'Georgia');

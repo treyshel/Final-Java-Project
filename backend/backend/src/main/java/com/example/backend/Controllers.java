813dc141944daf6f2419a19c86f99882303136a0
treyshel <tshelton@basecampcodingacademy.org>
2018-03-20 14:48:05 -0500

    got my first controller done.. working on bcrypt/salt to help with hashing passwords

--- backend/backend/src/main/java/com/example/backend/Controllers.java
+++ backend/backend/src/main/java/com/example/backend/Controllers.java
@@ -3,11 +3,25 @@ package com.example.backend;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 @RestController
 public class Controllers {
+    private String salt;
     @CrossOrigin()
+    @PostMapping("/signup")
+    public void signup(@RequestBody Student newStudent) {
+
+        StudentRepostiory.insertStudent(
+                newStudent.f_name,
+                newStudent.l_name,
+                newStudent.username,
+                newStudent.password,
+                newStudent.email
+        );
+    }
 }
--- backend/backend/src/main/java/com/example/backend/StudentRepostiory.java
+++ backend/backend/src/main/java/com/example/backend/StudentRepostiory.java
@@ -1,5 +1,8 @@
 package com.example.backend;
 
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,4 +11,23 @@ import java.util.ArrayList;
 
 public class StudentRepostiory {
 
+
+
+    public static boolean insertStudent(String f_name, String l_name, String username, String password, String email) {
+        try {
+            System.out.println(BCrypt.gensalt());
+            Connection con = Connect.connectDB();
+            PreparedStatement preparedStatement = con.prepareStatement("INSERT INTO players(f_name, l_name, username, password, email) VALUES (?, ?, ?, ?, ?);");
+            preparedStatement.setString(1, f_name);
+            preparedStatement.setString(2, l_name);
+            preparedStatement.setString(3, username);
+            preparedStatement.setString(4, password);
+            preparedStatement.setString(5, email);
+            preparedStatement.executeUpdate();
+            return true;
+        } catch (SQLException ex){
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
 }

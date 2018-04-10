8d0551011350de378eb8ae4f4963b2650afaeaed
Trey Shelton <tshelton@basecampcodingacademy.org>
2018-04-10 11:32:33 -0500

    can now hide and show info corresponding with the tab

--- frontend/feed/feed.js
+++ frontend/feed/feed.js
@@ -1,42 +1,57 @@
 PAGE_DATA = new Object();
 
-// **********change background color of active side tab***********
+// **********side tab functionality***********
 
-function sidebarProfile() {
-    $("#section1").click(function () {
-        $(".example1").hide(500);
-        $(".example2").hide(500);
-        $(".feedPage").show(500);
-    });
+$('#ProfileInfo').click(function() {
+    $('.postings').hide(500);
+    $('.connections').hide(500);
+    $('#networkConnections').removeClass('active');
+    $('#jobPostings').removeClass('active');
+    $('#ProfileInfoTab').addClass('active');
+    $('.feedPage').show(500);
+});
 
+$('#jobPostings').click(function() {
+    $('.feedPage').hide(500);
+    $('.connections').hide(500);
+    $('#networkConnections').removeClass('active');
+    $('#ProfileInfoTab').removeClass('active');
+    $('#jobPostings').addClass('active');
+    $('.postings').show(500);
+});
 
-    $("#section2").click(function () {
-        $(".feedPage").hide(500);
-        $(".example2").hide(500);
-        $(".example1").show(500);
-    });
-
-    $("#section3").click(function () {
-        $(".feedPage").hide(500);
-        $(".example1").hide(500);
-        $(".example2").show(500);
-    });
-}
-
+$('#networkConnections').click(function() {
+    $('.postings').hide(500);
+    $('.feedPage').hide(500);
+    $('#jobPostings').removeClass('active');
+    $('#ProfileInfoTab').removeClass('active');
+    $('#networkConnections').addClass('active');
+    $('.connections').show(500);
+});
 
 // ************shows feed************
 
 function showFeedData() {
     console.log(PAGE_DATA);
-    return "<h2>Name: " + PAGE_DATA.f_name + " " + PAGE_DATA.l_name + "</h2>" +
-        "<h2>Username: " + PAGE_DATA.username + "</h2>" +
-        "<h2>Email: " + PAGE_DATA.email + "</h2>"
+    return (
+        '<h2>Name: ' +
+        PAGE_DATA.f_name +
+        ' ' +
+        PAGE_DATA.l_name +
+        '</h2>' +
+        '<h2>Username: ' +
+        PAGE_DATA.username +
+        '</h2>' +
+        '<h2>Email: ' +
+        PAGE_DATA.email +
+        '</h2>'
+    );
 }
 
 function Feed(response) {
     console.log(response);
     PAGE_DATA = response;
-    $('.feedPage').html(showFeedData())
+    $('.feedPage').html(showFeedData());
 }
 
 function getParameterByUsername(username) {
@@ -49,15 +64,17 @@ function getParameterByUsername(username) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-$(function () {
+$(function() {
     var username = getParameterByUsername('username');
     console.log(username);
-    $.post('http://localhost:8080/feed/' + username).then(function handleResponse(response) {
-        var response = response;
-        console.log(response)
-        Feed(response);
-    })
-})
+    $.post('http://localhost:8080/feed/' + username).then(
+        function handleResponse(response) {
+            var response = response;
+            console.log(response);
+            Feed(response);
+        }
+    );
+});
 
 function logout() {
     var username = getParameterByUsername('username');
@@ -68,10 +85,7 @@ function logout() {
         crossDomain: true,
         contentType: 'application/json',
         mimeType: 'application/json'
-    }).then(function () {
-        window.location.replace(
-            '../signup-and-login/index.html'
-        )
-    })
+    }).then(function() {
+        window.location.replace('../signup-and-login/index.html');
+    });
 }
-

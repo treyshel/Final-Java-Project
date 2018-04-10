52569ae470c9700fb8e26231b801a969efb02fa5
Trey Shelton <tshelton@basecampcodingacademy.org>
2018-04-10 09:47:02 -0500

    just changed computers so I had to change computer username in my Connect class

---
+++ backend/backend/out/production/main/com/example/on-belay-tables.sql
@@ -0,0 +1,40 @@
+--table for the basecamp students
+CREATE TABLE student( 
+    id Serial Unique Primary Key,
+    session_key text,
+    f_name text,
+    l_name text,
+    username text,
+    p_word text,
+    email text 
+);
+
+
+--student's contact
+CREATE TABLE student_desc(
+    id Serial Unique Primary Key, 
+    student_id Integer references student(id),
+    linkedin_url text,
+    resume_url text,
+    github_url text,
+    portfolio_url text
+);
+
+--student's achievements
+CREATE TABLE student_achievements(
+    id Serial Unique Primary Key, 
+    student_id Integer references student(id),
+    programming_langs text,
+    bio text,
+    academics text,
+    desired_location text
+);
+
+-- INSERT INTO student(f_name, l_name, username, p_word, email) VALUES ('Trey', 'Shelton', 'treyshel', 'BASECAMP', 'tshelton@basecampcodingacademy.org');
+-- INSERT INTO student(f_name, l_name, username, p_word, email) VALUES ('Vale', 'Alvarez', 'valvarez', 'papaV', 'valvarez@basecampcodingacademy.org');
+
+-- INSERT INTO student_desc(student_id, linkedin_url, resume_url, github_url, portfolio_url) VALUES (1, 'https://www.linkedin.com/feed/', 'https://www.resume.com/trey/', 'https://www.github.com/treyshel/', 'https://www.treyshel.github.io');
+-- INSERT INTO student_desc(student_id, linkedin_url, resume_url, github_url, portfolio_url) VALUES (2, 'https://www.linkedin.com/feed/', 'https://www.resume.com/vale/', 'https://www.github.com/vale/', 'https://www.valvarez.github.io');
+
+-- INSERT INTO student_achievements(student_id, programming_langs, bio, academics, desired_location) VALUES (1, 'python', 'Software Developer', 'Pontotoc High', 'Mississippi');
+-- INSERT INTO student_achievements(student_id, programming_langs, bio, academics, desired_location) VALUES (2, 'java', 'Software Engineer', 'LHS', 'Georgia');
